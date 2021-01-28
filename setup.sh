594b082b3ff11b6f93ecdccbb6be308eb32e6fe5
Steve Constable <steve.constable@cyclicarx.com>
2021-01-28 16:37:11 -0500

    changes for Docker-only build

--- setup.sh
+++ setup.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	brew install node
-else
-	sudo apt install npm
-fi
-
-pip3 install virtualenv
-python3 -m venv env
-source env/bin/activate
 pip3 install -r requirements.txt
 python3 manage.py migrate
 npm install
